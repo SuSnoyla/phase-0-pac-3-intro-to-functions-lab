a6573047dd8cd0ad828336a5b1fe1b7c3f421e75
SuSnoyla <alyons.bz@gmail.com>
2022-12-29 12:48:45 -0800

    commit tree 0ab01b448870ab29b87c41ed9bec679b2513d774

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,6 +1,34 @@
 require ( './root.js' );
 
+function shout(string) {
+  return string.toUpperCase();
+}
 
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToHeadphonedRoommate(string) {
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!";
+}
+
+  if (string.toUpperCase() === string) {
+  return "YES INDEED!";
+ }
+
+  if (string === "Let\'s have dinner together!") {
+  return "I would love to!";
+ }
+}
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO');
